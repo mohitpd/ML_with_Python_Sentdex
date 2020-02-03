2fc848a5b7c3313dcf9674536f5ae20487bce554
mohitpd <mohitprasad28@gmail.com>
2020-02-04 01:54:35 +0530

    KNN

---
+++ KNearestNeighborsSKLearn.py
@@ -0,0 +1,25 @@
+import numpy as np
+from sklearn import preprocessing, neighbors
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('breast-cancer-wisconsin.data')
+df.replace('?', -99999, inplace=True)
+df.fillna(0, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+X = np.array(df.drop(['class'], 1,))
+y = np.array(df['class'])
+
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+print('accuracy = ', accuracy)
+
+example_measures = np.array([[4,2,1,2,2,2,1,2,1]])
+example_measures = example_measures.reshape(len(example_measures), -1)
+prediction = clf.predict(example_measures)
+print(prediction)
